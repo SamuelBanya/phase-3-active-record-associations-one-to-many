c9d01025f8b4c5c3c77a1bacd43efb2601cb4eb5
Samuel Banya <sbanya@fastmail.com>
2022-09-03 10:32:51 -0400

    Adding latest changes to assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,15 @@
 class Game < ActiveRecord::Base
-  
+  # NOTE: This uses the 'has_many' macro to establish the relationship that the single game
+  # has many reviews associated with it:
+  has_many :reviews
+
+  # NOTE: Without the 'has_many' Active Record macro, this would has been the resulting code:
+  # def reviews
+  # Review.where(game_id: self.id)
+  # end
+
+  # NOTE: This would have been the resulting SQL statements that would have been required
+  # SELECT "reviews."*
+  # FROM "reviews"
+  # WHERE "reviews"."game_id" = 1
 end
